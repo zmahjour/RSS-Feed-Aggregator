323ee1ace7d23f8d06874804fd3157d61f8c371d
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-14 20:58:18 +0330

    chore: define Subscription model

--- interactions/models.py
+++ interactions/models.py
@@ -4,3 +4,12 @@ from accounts.models import User
 from podcasts.models import Channel, Episode
 
 
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} subscribed {self.channel}"
+
+
