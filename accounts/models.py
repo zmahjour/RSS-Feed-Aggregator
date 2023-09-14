35cb5ab301bf04734c86cb5c3f3109bbb51b04b6
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-14 19:44:36 +0330

    chore: define User model inhertied from AbstractBaseUser and PermissionMixin

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    joined = models.DateTimeField(auto_now_add=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "name"]
+
+    def __str__(self):
+        return self.username
+
+
