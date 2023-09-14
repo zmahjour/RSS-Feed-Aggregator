c000f6aef3e2e7f1b16daa8f88138e50519c56a3
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-14 20:56:25 +0330

    chore: add str method to Category model

--- podcasts/models.py
+++ podcasts/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.title
+
 
 class Channel(models.Model):
     title = models.CharField(max_length=255)
