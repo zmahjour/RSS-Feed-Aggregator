bcc65a7578405698d1f8bb5a2b5360da03525ece
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-14 19:46:49 +0330

    chore: define UserManager class inherited from BaseUserManager

--- accounts/managers.py
+++ accounts/managers.py
@@ -1,3 +1,25 @@
 from django.contrib.auth.models import BaseUserManager
 
 
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password, **other_fields):
+        if not username:
+            raise ValueError("Users must have an username")
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            username=username, email=self.normalize_email(email), **other_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password, **other_fields):
+        user = self.create_user(
+            username=username, email=email, password=password, **other_fields
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
