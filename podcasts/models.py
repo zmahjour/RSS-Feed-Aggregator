6851e3af58614977f62263249580ac663edb75e9
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-23 17:15:20 +0330

    refactor: edit Channel model

--- podcasts/models.py
+++ podcasts/models.py
@@ -9,17 +9,17 @@ class Category(models.Model):
 
 
 class Channel(models.Model):
+    rss_url = models.URLField(unique=True)
     title = models.CharField(max_length=255)
     subtitle = models.TextField(null=True, blank=True)
     description = models.TextField()
-    author = models.CharField(max_length=100)
-    pubdate = models.DateTimeField(null=True, blank=True)
-    language = models.CharField(max_length=10)
-    owner_name = models.CharField(max_length=100)
-    owner_email = models.CharField(max_length=255)
-    image_url = models.TextField(null=True, blank=True)
-    rss_feed = models.TextField()
-    categories = models.ManyToManyField(Category)
+    author = models.CharField(max_length=100, null=True, blank=True)
+    pub_date = models.DateTimeField(null=True, blank=True)
+    language = models.CharField(max_length=25, null=True, blank=True)
+    owner_name = models.CharField(max_length=100, null=True, blank=True)
+    owner_email = models.CharField(max_length=255, null=True, blank=True)
+    image_url = models.URLField(null=True, blank=True)
+    categories = models.ManyToManyField(Category, blank=True)
 
     def __str__(self):
         return self.title
