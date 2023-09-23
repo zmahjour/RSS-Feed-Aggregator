72fe9e98a8afc33712f9d3127500fec0330bdcc4
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-23 17:13:58 +0330

    refactor: edit User model

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,7 @@ from .managers import UserManager
 
 class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(max_length=255, unique=True)
+    email = models.EmailField(unique=True)
     joined = models.DateTimeField(auto_now_add=True)
 
     is_active = models.BooleanField(default=True)
@@ -15,7 +15,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username", "name"]
+    REQUIRED_FIELDS = ["username"]
 
     def __str__(self):
         return self.username
