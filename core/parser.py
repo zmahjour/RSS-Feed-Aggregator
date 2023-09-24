fb2e51b41eee820e750231517aca352859994ba9
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-24 10:51:58 +0330

    chore: write a function to get channel data in two dicts

--- core/parser.py
+++ core/parser.py
@@ -11,3 +11,29 @@ def get_rss_text(rss_url):
     return rss_text
 
 
+def get_channel_data(rss_text):
+    root = ET.fromstring(rss_text)
+    channel_data = {}
+    channel_data_attrs = {}
+    categories = "{http://www.itunes.com/dtds/podcast-1.0.dtd}category"
+    channel_data_attrs[categories] = []
+
+    for element in root.find("channel").iter():
+        element_text = None
+        if element.tag == "item":
+            break
+
+        if element.tag != "channel":
+            if element.text:
+                element_text = element.text.strip()
+            channel_data[element.tag] = element_text
+
+            if element.attrib:
+                if element.tag == categories:
+                    category = element.attrib.get("text")
+                    channel_data_attrs[categories].append(category)
+                else:
+                    channel_data_attrs[element.tag] = element.attrib
+
+    return channel_data, channel_data_attrs
+
