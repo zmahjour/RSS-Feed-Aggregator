98a2f5e2e1a93fa0914f219463a209d72a07149e
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-24 17:42:31 +0330

    chore: import required things

--- podcasts/views.py
+++ podcasts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .serializers import RssSerializer
+from core.parser import create_update_channel_episodes
+
+
