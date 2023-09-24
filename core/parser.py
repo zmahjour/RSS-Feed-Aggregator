89291c8145a51b37ec8866d5383b967baf452248
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-24 10:51:19 +0330

    chore: write a function to get rss text from rss url

--- core/parser.py
+++ core/parser.py
@@ -4,3 +4,10 @@ from datetime import datetime
 
 from podcasts.models import Category, Channel, Episode
 
+
+def get_rss_text(rss_url):
+    response = requests.get(rss_url)
+    rss_text = response.text
+    return rss_text
+
+
