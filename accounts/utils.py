226f0a49e364dffa71fcddd3e62e57e2af4e6c41
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-28 00:16:27 +0330

    chore: define generate_access_token method

--- accounts/utils.py
+++ accounts/utils.py
@@ -7,3 +7,16 @@ import datetime
 class JWTToken:
     jti = str(uuid.uuid4())
 
+    def generate_access_token(self, user):
+        access_token_payload = {
+            "user_id": user.id,
+            "exp": datetime.datetime.now() + datetime.timedelta(days=1),
+            "iat": datetime.datetime.now(),
+            "jti": self.jti,
+        }
+
+        access_token = jwt.encode(
+            access_token_payload, settings.SECRET_KEY, algorithm="HS256"
+        )
+        return access_token
+
