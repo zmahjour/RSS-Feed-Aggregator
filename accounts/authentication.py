deeb8b4d908522e0c51c3aa822f5ff5bd39fc1dd
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-28 00:04:40 +0330

    chore: define EmailBackend class inherited from BaseBackend

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -7,3 +7,21 @@ import jwt
 from .models import User
 
 
+class EmailBackend(BaseBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = User.objects.get(email=username)
+        except User.DoesNotExist:
+            return None
+
+        if user.check_password(password):
+            return user
+        return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+
+
