9294937eff4325050b04d428ea2a1992760e415d
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-28 20:01:12 +0330

    style: change p to P

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # project apps
+    # Project apps
     "core.apps.CoreConfig",
     "podcasts.apps.PodcastsConfig",
     "interactions.apps.InteractionsConfig",
