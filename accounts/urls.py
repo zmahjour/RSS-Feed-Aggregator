daec60305624f41a05b54d55fa9c22da7aab20a2
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-28 19:52:17 +0330

    chore: define urls for register, login, logout and create new access and refresh token

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 app_name = "accounts"
 urlpatterns = [
+    path("register/", views.UserRegisterView.as_view()),
+    path("login/", views.UserLoginView.as_view()),
+    path("logout/", views.UserLogoutView.as_view()),
+    path("token/access/", views.ObtainAccessToken.as_view()),
 ]
