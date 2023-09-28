26c55cdf0d80b8b41376567fad9157cf9f236b61
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-28 21:58:20 +0330

    refactor: remove second return and assign True to raise_exception

--- podcasts/views.py
+++ podcasts/views.py
@@ -9,11 +9,10 @@ from .tasks import create_or_update
 class RssView(APIView):
     def post(self, request):
         serialized_data = RssSerializer(data=request.data)
-        if serialized_data.is_valid():
+        if serialized_data.is_valid(raise_exception=True):
             rss_url = serialized_data.validated_data["rss_url"]
             create_or_update.apply_async(kwargs={"rss_url": rss_url})
             return Response(
                 data={"message": "Podcast updated successfully."},
                 status=status.HTTP_201_CREATED,
             )
-        return Response(data=serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
