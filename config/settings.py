9821af3ff3600fce23afbd18e8fdb5ff742e3242
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-28 20:04:59 +0330

    chore: define authentication backends

--- config/settings.py
+++ config/settings.py
@@ -146,3 +146,8 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.User"
+
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "accounts.authentication.EmailBackend",
+]
