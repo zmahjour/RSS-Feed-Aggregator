7dba7ae28c7b71b0e4b3b02c191bb1f9eccb2e54
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-28 20:02:39 +0330

    chore: config database to use postgresql as default

--- config/settings.py
+++ config/settings.py
@@ -87,8 +87,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
+    }
+}
     }
 }
 
