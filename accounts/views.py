3f0bb2b5479d0b9a5dbd46e6f7e1786a1f9f3a0b
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-09-28 19:55:14 +0330

    chore: import required things

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from django.contrib.auth import authenticate
+from django.core.cache import cache
+from django.conf import settings
+import jwt
+from .serializers import UserRegisterSerializer, UserLoginSerializer
+from .utils import JWTToken
+from .models import User
 
-# Create your views here.
+
