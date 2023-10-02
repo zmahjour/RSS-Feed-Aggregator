dafbde8f33fc426f8b0e9c207f8802de59557bb1
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-10-02 23:30:43 +0330

    refactor: edit based on other changes

--- podcasts/tasks.py
+++ podcasts/tasks.py
@@ -1,11 +1,11 @@
 from celery import shared_task
-from core.parser import create_or_update_channel_and_episodes
+from .parser import create_or_update
 
 
 @shared_task(bind=True, max_retries=4, retry_backoff=4)
-def create_or_update(self, rss_url):
+def create_or_update_task(self, rss_url):
     try:
-        create_or_update_channel_and_episodes(rss_url=rss_url)
+        create_or_update(rss_url=rss_url)
     except Exception as e:
         print(e)
         self.retry()
