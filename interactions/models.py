1ea805eb75d7d461c56e5d6eac488bf8e2f40922
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-10-02 23:26:46 +0330

    refactor: define Bookmark model

--- interactions/models.py
+++ interactions/models.py
@@ -59,3 +59,20 @@ class Playlist(models.Model):
         return self.title
 
 
+class Bookmark(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    playlist = models.ForeignKey(
+        Playlist, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["content_type", "object_id"]),
+        ]
+
+    def __str__(self):
+        return self.id
