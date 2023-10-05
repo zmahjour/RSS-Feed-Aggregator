e9072e1ae2ec67d82fca10368d76489848e4b324
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-10-05 08:57:08 +0330

    refactor: import IsAdminUser

--- podcasts/views.py
+++ podcasts/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAdminUser
 from django.db import transaction
 from .serializers import RssSerializer
 from .models import Rss
