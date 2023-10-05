11174b257c7e6b6b834b6985982e5ef765e43be1
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-10-05 14:50:34 +0330

    refactor: add some commented code

--- podcasts/tasks.py
+++ podcasts/tasks.py
@@ -4,14 +4,28 @@ from .models import Rss
 from .parser import create_or_update
 
 
+logger = get_task_logger(__name__)
+
+
+# class BaseTaskWithRetry(Task):
+#     autoretry_for = (TypeError,)
+#     max_retries = 5
+#     retry_backoff = True
+#     retry_backoff_max = 700
+#     retry_jitter = False
+
+
 @shared_task
 def read_rss_data_task():
     rss_urls = [rss.rss_url for rss in Rss.objects.all()]
     return rss_urls
 
 
+# @shared_task(bind=True, max_retries=4, retry_backoff=4)
+@shared_task
+def create_or_update_task(rss_url):
     try:
         create_or_update(rss_url=rss_url)
     except Exception as e:
-        print(e)
-        self.retry()
+        logger.info(e)
+        # self.retry()
