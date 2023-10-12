6a6abf6bff3c03b4e40544865036a8541e7f50fa
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-10-12 11:20:38 +0330

    chore: import and register models

--- podcasts/admin.py
+++ podcasts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Rss, Category, Channel, Episode
 
-# Register your models here.
+
+admin.site.register(Rss)
+admin.site.register(Category)
+admin.site.register(Channel)
+admin.site.register(Episode)
