d92494774c867ff4059841e84c0fb4c52fe34a1e
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-10-12 11:16:27 +0330

    refacor: delete read_rss_data_task, edit create_or_update_task

--- podcasts/tasks.py
+++ podcasts/tasks.py
@@ -13,17 +13,6 @@ class BaseTaskWithRetry(Task):
     retry_jitter = False
 
 
-@shared_task
-def read_rss_data_task():
-    rss_urls = [rss.rss_url for rss in Rss.objects.all()]
-    return rss_urls
-
-
-# @shared_task(bind=True, max_retries=4, retry_backoff=4)
-@shared_task
+@shared_task(base=BaseTaskWithRetry)
 def create_or_update_task(rss_url):
-    try:
-        create_or_update(rss_url=rss_url)
-    except Exception as e:
-        logger.info(e)
-        # self.retry()
+    create_or_update(rss_url=rss_url)
