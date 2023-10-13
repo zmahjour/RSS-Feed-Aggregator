f9205f27117400666bc88326608c90e498193e23
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-10-13 08:11:45 +0330

    refactor: call create_or_update_task in CreateOrUpdateView

--- podcasts/views.py
+++ podcasts/views.py
@@ -26,14 +26,7 @@ class CreateOrUpdateView(APIView):
     permission_classes = [IsAdminUser]
 
     def get(self, request):
-        rss_urls = [rss.rss_url for rss in Rss.objects.all()]
-        for rss_url in rss_urls:
-            with transaction.atomic():
-                try:
-                    create_or_update_task.delay(rss_url=rss_url)
-                    print(f"Podcast with '{rss_url}' url updated successfully.")
-                except Exception as e:
-                    print(f"error: {str(e)}")
+        create_or_update_task.delay()
         return Response(
             data={"message": "All podcasts have been updated."},
             status=status.HTTP_200_OK,
