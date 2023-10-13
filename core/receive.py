9d43ff3577c0a59b1175e1eb83b63e7fd4b418c4
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-10-13 08:21:34 +0330

    chore: define consumer function

--- core/receive.py
+++ core/receive.py
@@ -3,3 +3,11 @@ import json
 from interactions.models import Notification
 from accounts.models import User
 
+def consumer(queue):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
+    channel = connection.channel()
+    channel.queue_declare(queue=queue)
+    channel.basic_consume(queue=queue, on_message_callback=notif_callback)
+
+    print(" [*] Waiting for messages. To exit press CTRL+C")
+    channel.start_consuming()
