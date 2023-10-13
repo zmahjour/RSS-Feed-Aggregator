3fd8c882fd2aed5e37a41a7b6d93dd3c9f80f6fc
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-10-13 08:24:48 +0330

    chore: difine notif_callback function

--- core/receive.py
+++ core/receive.py
@@ -3,6 +3,16 @@ import json
 from interactions.models import Notification
 from accounts.models import User
 
+
+def notif_callback(ch, method, properties, body):
+    body = json.loads(body)
+    user = User.objects.get(username=body["username"])
+    Notification.objects.create(
+        user=user, action=body["action"], content=body["notification"]
+    )
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
 def consumer(queue):
     connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
     channel = connection.channel()
