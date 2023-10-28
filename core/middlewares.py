42cebb165f2490a51eda2aec22fa94eba2d0c3be
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-10-28 11:36:27 +0330

    chore: define LoggingMiddleware class

--- core/middlewares.py
+++ core/middlewares.py
@@ -4,3 +4,8 @@ import pytz
 import json
 
 
+class LoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.logger = logging.getLogger("api_logger")
+
