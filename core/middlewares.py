784c43f536a8c66b5aeadecb8fbd7eab75a2bb79
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-10-28 11:37:00 +0330

    chore: define __call__ method in LoggingMiddleware class

--- core/middlewares.py
+++ core/middlewares.py
@@ -9,3 +9,12 @@ class LoggingMiddleware:
         self.get_response = get_response
         self.logger = logging.getLogger("api_logger")
 
+    def __call__(self, request):
+        request_data = self.get_request_data(request)
+        response = self.get_response(request)
+        response_data = self.get_response_data(response)
+        log_data = {"request": request_data, "response": response_data}
+        self.logger.info(json.dumps(log_data))
+
+        return response
+
