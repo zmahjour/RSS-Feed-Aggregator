83eb2716a5be8db6e32048571939a61463583bd5
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-10-28 11:46:49 +0330

    chore: define ElasticsearchHandler class

--- core/logging_handlers.py
+++ core/logging_handlers.py
@@ -5,3 +5,13 @@ import logging
 import pytz
 import json
 
+
+class ElasticsearchHandler(logging.Handler):
+    def __init__(self, prefix, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.prefix = prefix
+        self.es = Elasticsearch(
+            f"http://{settings.ELASTICSEARCH_HOST}:{settings.ELASTICSEARCH_PORT}"
+        )
+
