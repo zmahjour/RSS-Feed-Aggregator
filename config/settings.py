c9eb2cbf754a89bd887147c01124fcbd0cadca22
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-10-28 11:40:26 +0330

    chore: add LoggingMiddleware to settings

--- config/settings.py
+++ config/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "core.middlewares.LoggingMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
