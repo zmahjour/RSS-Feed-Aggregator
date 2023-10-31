6de4189c67c5389631e6795167621388a3865614
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-10-31 18:55:11 +0330

    chore: import Channel and Episode models and define ChannelSerializer and EpisodeSerializer

--- podcasts/serializers.py
+++ podcasts/serializers.py
@@ -1,5 +1,18 @@
 from rest_framework import serializers
+from .models import Channel, Episode
 
 
 class RssSerializer(serializers.Serializer):
     rss_url = serializers.CharField()
+
+
+class ChannelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Channel
+        fields = "__all__"
+
+
+class EpisodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Episode
+        fields = "__all__"
