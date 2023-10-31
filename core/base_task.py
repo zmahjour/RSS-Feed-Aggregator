c45b4790c08e73f2f3627501291ce1c1fbb97c41
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-10-31 18:38:34 +0330

    refacotr: move BaseTaskWithRetry class to base_task file

--- core/base_task.py
+++ core/base_task.py
@@ -2,3 +2,10 @@ from celery import Task, states
 import logging
 import json
 
+
+class BaseTaskWithRetry(Task):
+    autoretry_for = (Exception,)
+    max_retries = 5
+    retry_backoff = 2
+    retry_jitter = False
+
