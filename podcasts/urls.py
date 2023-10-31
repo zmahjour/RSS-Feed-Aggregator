9fbf768580108e031702538c4c559f360425f838
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-10-31 18:58:09 +0330

    chore: define required urls

--- podcasts/urls.py
+++ podcasts/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = "podcasts"
 urlpatterns = [
     path("rss/", views.RssView.as_view()),
-    path("update/", views.CreateOrUpdateView.as_view()),
+    path("update/one/", views.CreateOrUpdateOneChannelView.as_view()),
+    path("update/all/", views.CreateOrUpdateAllChannelsView.as_view()),
+    path("channels/", views.ListOfChannelsView.as_view()),
+    path("<int:channel_id>/episodes/", views.ListOfEpisodesView.as_view()),
 ]
