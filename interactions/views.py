384249cfd12df8b9008c1f8f646aa62e94d5d32c
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-01 13:10:01 +0330

    chore: define UnikeEpisodeView to unlike episodes

--- interactions/views.py
+++ interactions/views.py
@@ -31,3 +31,26 @@ class LikeEpisodeView(APIView):
         )
 
 
+class UnlikeEpisodeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, episode_id):
+        content_type = get_object_or_404(ContentType, model="episode")
+        user = request.user
+
+        if not Like.is_liked(
+            user=user, content_type=content_type, object_id=episode_id
+        ):
+            return Response(
+                data={"message": _("You have not liked this episode before.")},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        Like.objects.get(
+            user=user, content_type=content_type, object_id=episode_id
+        ).delete()
+
+        return Response(
+            data={"message": _("You have unliked this episode.")},
+            status=status.HTTP_200_OK,
+        )
