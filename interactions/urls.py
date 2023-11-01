1b2fba8cd245d5439e4031f2064d53635ea7564e
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-01 13:10:35 +0330

    chore: define urls for like and unlike episodes

--- interactions/urls.py
+++ interactions/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 app_name = "interactions"
 urlpatterns = [
+    path("like/<int:episode_id>/", views.LikeEpisodeView.as_view()),
+    path("unlike/<int:episode_id>/", views.UnlikeEpisodeView.as_view()),
 ]
