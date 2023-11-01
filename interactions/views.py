5787183c01e1023e9c89f027ad9ac03f385a7c36
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-01 13:08:56 +0330

    chore: import required modules

--- interactions/views.py
+++ interactions/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.utils.translation import gettext_lazy as _
+from django.contrib.contenttypes.models import ContentType
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from podcasts.models import Episode
+from .models import Like
 
-# Create your views here.
+
