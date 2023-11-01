4fc5619823f5590258fd715376dd99669cedc305
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-01 13:06:32 +0330

    refactor: rename Favorite model to Like

--- interactions/admin.py
+++ interactions/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Subscription, Favorite, Comment, Playlist, Bookmark, Notification
+from .models import Subscription, Like, Comment, Playlist, Bookmark, Notification
 
 
 admin.site.register(Subscription)
-admin.site.register(Favorite)
+admin.site.register(Like)
 admin.site.register(Comment)
 admin.site.register(Playlist)
 admin.site.register(Bookmark)
