a400feaa97d8e75b9d781a1946c3d7a6b58836ff
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-02 10:49:39 +0330

    chore: define CommentSerializer class

--- interactions/serializers.py
+++ interactions/serializers.py
@@ -1,3 +1,8 @@
 from rest_framework import serializers
 from .models import Comment
 
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ["user", "content", "created", "updated"]
