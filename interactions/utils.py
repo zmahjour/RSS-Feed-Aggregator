bf5c4b40c6234306f4cddd469ff6c29ea4591f68
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-02 10:51:19 +0330

    chore: import required modules and classes

---
+++ interactions/utils.py
@@ -0,0 +1,5 @@
+from django.contrib.contenttypes.models import ContentType
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status
+
