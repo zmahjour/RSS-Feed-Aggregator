a6a7088f7d0eb01b719871ca09a9e7bbec237bb8
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-02 10:53:57 +0330

    chore: define create_interaction_with_generic_relation fucntion

--- interactions/utils.py
+++ interactions/utils.py
@@ -3,3 +3,27 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 
+
+def create_interaction_with_generic_relation(
+    user,
+    object_model,
+    object_id,
+    interaction_model,
+    failure_message,
+    success_message,
+    is_method=None,
+    **kwargs
+):
+    content_type = ContentType.objects.get_for_model(object_model)
+    object = get_object_or_404(object_model, pk=object_id)
+
+    if is_method and is_method(user, content_type, object_id):
+        return Response(
+            data={"message": failure_message}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    interaction_model.objects.create(user=user, content_object=object, **kwargs)
+
+    return Response(data={"message": success_message}, status=status.HTTP_201_CREATED)
+
+
